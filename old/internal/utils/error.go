a018dca77607a9a3bcbcb11ec4e09ad49a336734
Ari Palo <ari.palo@almamedia.fi>
2021-10-18 16:52:16 +0300

    refactor: delete obsolete fiels

--- old/internal/utils/error.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-func PrintError(err error) {
-	SafeLogLn()
-	SafeLogLn(TextGrayDark(CreateRuler("=")))
-	SafeLogLn(FormatMessage(COLOR_ERROR, "🚨 ", "credential_process", "Failed"))
-	SafeLogLn(FormatMessage(COLOR_DEBUG, "ℹ️  ", "credential_process", "\n\n"+err.Error()))
-	SafeLogLn(TextGrayDark(CreateRuler("=")))
-}
