b50b6e9e0424a6867fdd93aa56dab01859a71be3
Ari Palo <ari.palo@almamedia.fi>
2021-10-23 18:38:30 +0300

    fix: check err

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/aripalo/aws-mfa-credential-process/cmd"
+import (
+	"log"
+	"os"
+	"runtime/pprof"
+
+	"github.com/aripalo/aws-mfa-credential-process/cmd"
+)
 
 func main() {
+
+	cpuProfile := os.Getenv("CPU_PROFILE")
+
+	if cpuProfile != "" {
+		f, err := os.Create(cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = pprof.StartCPUProfile(f)
+		if err != nil {
+			panic(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 	cmd.Execute()
 }
