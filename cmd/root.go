2bec522b1974266b8fa021962220c0634a43a958
Ari Palo <ari.palo@almamedia.fi>
2021-10-23 18:33:42 +0300

    chore: remove obsolete stuff

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var cfgFile string
 var version string = "development"
 
 // rootCmd represents the base command when called without any subcommands
@@ -23,7 +22,6 @@ func Execute() {
 }
 
 func init() {
-	//cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().Bool(
 		config.Defaults.NoColor.Name,
@@ -43,31 +41,4 @@ func init() {
 		config.Defaults.Debug.Usage,
 	)
 
-	//rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.newApp.yaml)")
 }
-
-// initConfig reads in config file and ENV variables if set.
-/*
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".newApp" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".newApp")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
-}
-*/
