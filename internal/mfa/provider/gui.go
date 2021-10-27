7e8a8c01328782dd0a016cc7159fd2f8146941cd
Ari Palo <ari.palo@almamedia.fi>
2021-10-27 15:59:46 +0300

    test: test gui mfa provider

--- internal/mfa/provider/gui.go
+++ internal/mfa/provider/gui.go
@@ -7,13 +7,15 @@ import (
 	"github.com/aripalo/vegas-credentials/internal/prompt"
 )
 
+var guiPrompt = prompt.Dialog
+
 func (t *TokenProvider) QueryGUI(ctx context.Context, a interfaces.AssumeCredentialProcess) {
 	var token Token
 	var err error
 
 	token.Provider = TOKEN_PROVIDER_GUI_DIALOG_PROMPT
 
-	value, err := prompt.Dialog(ctx, "Multifactor Authentication", "Enter TOPT MFA Token Code:")
+	value, err := guiPrompt(ctx, "Multifactor Authentication", "Enter TOPT MFA Token Code:")
 	if err != nil {
 		t.errorChan <- &err
 	} else {
