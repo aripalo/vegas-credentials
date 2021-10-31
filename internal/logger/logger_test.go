ff49c497970f92555433dd44e262611ba8f06c15
Ari Palo <ari.palo@almamedia.fi>
2021-10-31 18:34:28 +0200

    test: formatted f-functions

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -19,7 +19,7 @@ type LoggerTestCase struct {
 	emoji       string
 	prefix      string
 	message     string
-	args        []interface{}
+	args        []string
 	fn          func(a interfaces.AssumeCredentialProcess, emoji string, prefix string, message string)
 	fnF         func(a interfaces.AssumeCredentialProcess, emoji string, prefix string, message string, args ...interface{})
 	want        string
@@ -29,26 +29,129 @@ func TestLogger(t *testing.T) {
 
 	tests := []LoggerTestCase{
 		{
-			description: "non-verbose debugf",
+			description: "non-verbose Debugln",
 			flags: config.Flags{
 				Verbose: false,
 			},
 			emoji:   "🚧",
 			prefix:  "Test",
 			message: "Message",
-			fnF:     Debugf,
+			fn:      Debugln,
 			want:    "",
 		},
 		{
-			description: "verbose debugf",
+			description: "verbose Debugln",
 			flags: config.Flags{
 				Verbose: true,
 			},
 			emoji:   "🚧",
 			prefix:  "Test",
 			message: "Message",
-			fnF:     Debugf,
+			fn:      Debugln,
+			want:    "🚧 \x1b[90m\x1b[1mTest:\x1b[0m\x1b[0m \x1b[90mMessage\x1b[0m\n",
+		},
+		{
+			description: "non-verbose Debug",
+			flags: config.Flags{
+				Verbose: false,
+			},
+			emoji:   "🚧",
+			prefix:  "Test",
+			message: "Message",
+			fn:      Debug,
+			want:    "",
+		},
+		{
+			description: "verbose Debug",
+			flags: config.Flags{
+				Verbose: true,
+			},
+			emoji:   "🚧",
+			prefix:  "Test",
+			message: "Message",
+			fn:      Debug,
 			want:    "🚧 \x1b[90m\x1b[1mTest:\x1b[0m\x1b[0m \x1b[90mMessage\x1b[0m",
+		},
+		{
+			description: "non-verbose Debugf",
+			flags: config.Flags{
+				Verbose: false,
+			},
+			emoji:   "🚧",
+			prefix:  "Test",
+			message: "Message %s",
+			args:    []string{"formatted"},
+			fnF:     Debugf,
+			want:    "",
+		},
+		{
+			description: "verbose Debugf",
+			flags: config.Flags{
+				Verbose: true,
+			},
+			emoji:   "🚧",
+			prefix:  "Test",
+			message: "Message %s",
+			args:    []string{"formatted"},
+			fnF:     Debugf,
+			want:    "🚧 \x1b[90m\x1b[1mTest:\x1b[0m\x1b[0m \x1b[90mMessage formatted\x1b[0m",
+		},
+		{
+			description: "Infoln",
+			flags:       config.Flags{},
+			emoji:       "🚧",
+			prefix:      "Test",
+			message:     "Message",
+			fn:          Infoln,
+			want:        "🚧 \x1b[90m\x1b[1mTest:\x1b[0m\x1b[0m \x1b[90mMessage\x1b[0m\n",
+		},
+		{
+			description: "Info",
+			flags:       config.Flags{},
+			emoji:       "🚧",
+			prefix:      "Test",
+			message:     "Message",
+			fn:          Info,
+			want:        "🚧 \x1b[90m\x1b[1mTest:\x1b[0m\x1b[0m \x1b[90mMessage\x1b[0m",
+		},
+		{
+			description: "Infof",
+			flags:       config.Flags{},
+			emoji:       "🚧",
+			prefix:      "Test",
+			message:     "Message %s",
+			args:        []string{"formatted"},
+			fnF:         Infof,
+			want:        "🚧 \x1b[90m\x1b[1mTest:\x1b[0m\x1b[0m \x1b[90mMessage formatted\x1b[0m",
+		},
+
+		{
+			description: "Successln",
+			flags:       config.Flags{},
+			emoji:       "🚧",
+			prefix:      "Test",
+			message:     "Message",
+			fn:          Successln,
+			want:        "🚧 \x1b[32m\x1b[1mTest:\x1b[0m\x1b[0m \x1b[32mMessage\x1b[0m\n",
+		},
+		{
+			description: "Success",
+			flags:       config.Flags{},
+			emoji:       "🚧",
+			prefix:      "Test",
+			message:     "Message",
+			fn:          Success,
+			want:        "🚧 \x1b[32m\x1b[1mTest:\x1b[0m\x1b[0m \x1b[32mMessage\x1b[0m",
+		},
+		{
+			description: "Successf",
+			flags:       config.Flags{},
+			emoji:       "🚧",
+			prefix:      "Test",
+			message:     "Message %s",
+			args:        []string{"formatted"},
+			fnF:         Successf,
+			want:        "🚧 \x1b[32m\x1b[1mTest:\x1b[0m\x1b[0m \x1b[32mMessage formatted\x1b[0m",
 		},
 	}
 
@@ -84,7 +187,12 @@ func TestLogger(t *testing.T) {
 			}
 
 			if tc.fnF != nil {
-				tc.fnF(a, tc.emoji, tc.prefix, tc.message, tc.args...)
+				b := make([]interface{}, len(tc.args))
+				for i := range tc.args {
+					b[i] = tc.args[i]
+				}
+
+				tc.fnF(a, tc.emoji, tc.prefix, tc.message, b...)
 			}
 
 			if tc.fn != nil {
