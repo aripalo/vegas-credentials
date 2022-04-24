66fb2ea6b654a053649f7f7a62dbfcac6ab7c6ff
Ari Palo <ari.palo@almamedia.fi>
2022-04-25 00:32:20 +0300

    feat: mutex control

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/aripalo/vegas-credentials/cmd"
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/aripalo/vegas-credentials/cmd"
+	"github.com/aripalo/vegas-credentials/internal/locations"
+	"github.com/aripalo/vegas-credentials/internal/mutex"
+	"github.com/aripalo/vegas-credentials/internal/utils"
+)
+
+const lockFile string = "mutex-lock"
 
 func main() {
+	lockPath := filepath.Join(locations.StateDir, lockFile)
+	mc, err := mutex.New(lockPath)
+	if err != nil {
+		utils.Bail(fmt.Sprintf("Configuration Error: %s", err))
+	}
+	mc.Lock()
+	defer mc.Unlock()
+
 	cmd.Execute()
 }
