0091c7c574bd4a3c1ba741cc93406394b96793fb
Ari Palo <ari.palo@almamedia.fi>
2022-04-25 00:44:01 +0300

    feat: assume implementation

--- internal/application/assume.go
+++ internal/application/assume.go
@@ -1,14 +1,73 @@
 package application
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"path/filepath"
+
+	"github.com/aripalo/vegas-credentials/internal/assumable"
+	"github.com/aripalo/vegas-credentials/internal/cache"
+	"github.com/aripalo/vegas-credentials/internal/credentials"
+	"github.com/aripalo/vegas-credentials/internal/database"
+	"github.com/aripalo/vegas-credentials/internal/locations"
+	"github.com/aripalo/vegas-credentials/internal/msg"
+	"github.com/aripalo/vegas-credentials/internal/totp"
+	"github.com/aripalo/vegas-credentials/internal/utils"
+	"github.com/dustin/go-humanize"
+)
 
 type AssumeFlags struct {
 	Profile string `mapstructure:"profile"`
 }
 
-func (a *App) Assume(flags AssumeFlags) error {
+var cacheLocation string = filepath.Join(locations.CacheDir, "session-cache")
 
-	fmt.Println("TODO!", flags)
+func (app *App) Assume(flags AssumeFlags) error {
+
+	a, err := assumable.New(locations.AwsConfig, flags.Profile)
+	if err != nil {
+		utils.Bail(fmt.Sprintf("Credentials: Error: %s", err))
+	}
+
+	msg.Message.Debugln("ℹ️", fmt.Sprintf("Credentials: Role: %s", a.RoleArn))
+
+	db, err := database.Open(cacheLocation, database.DatabaseOptions{})
+	if err != nil {
+		utils.Bail(fmt.Sprintf("Configuration Error: %s", err))
+	}
+
+	creds := credentials.New(cache.New(db), a)
+
+	if err = creds.FetchFromCache(); err != nil {
+		msg.Message.Debugln("ℹ️", fmt.Sprintf("Credentials: Cached: %s", err))
+		msg.Message.Debugln("ℹ️", "Credentials: STS: Fetching...")
+		msg.Message.Debugln("ℹ️", fmt.Sprintf("MFA: TOTP: %s", a.MfaSerial))
+
+		t := totp.New(totp.TotpOptions{
+			YubikeySerial: a.YubikeySerial,
+			YubikeyLabel:  a.YubikeyLabel,
+			EnableGui:     true,
+		})
+
+		err = creds.FetchFromAWS(t.Get)
+		if err != nil {
+			if errors.Is(err, context.DeadlineExceeded) {
+				utils.Bail(fmt.Sprintf("Operation Timeout"))
+			}
+			utils.Bail(fmt.Sprintf("Credentials: STS: %s", err))
+		} else {
+			msg.Message.Successln("✅", fmt.Sprintf("Credentials: STS: Received fresh credentials"))
+			msg.Message.Infoln("⏳", fmt.Sprintf("Credentials: STS: Expiration in %s", humanize.Time(creds.Expiration)))
+		}
+	} else {
+		msg.Message.Successln("✅", "Credentials: Cached: Received")
+		msg.Message.Infoln("⏳", fmt.Sprintf("Credentials: Cached: Expiration in %s", humanize.Time(creds.Expiration)))
+	}
+
+	msg.Message.HorizontalRuler()
+
+	creds.Output()
 
 	return nil
 }
