8ed98c39f8080f558df063a48fdef65edb3a6443
Ari Palo <ari.palo@almamedia.fi>
2022-04-25 00:19:46 +0300

    remove old implementation

--- internal/config/files.go
+++
@@ -1,6 +0,0 @@
-package config
-
-// Describes the path for a file used to control parallelism via file locks.
-var MutexLockFile string = func(filename string) string {
-	return TempFilePath(filename)
-}("mutex-lock")
--- internal/logger/support.go
+++
@@ -1,9 +0,0 @@
-package logger
-
-import "fmt"
-
-const issueUrl string = "https://github.com/aripalo/vegas-credentials/issues/new"
-
-func GetSupportString(padding string) string {
-	return fmt.Sprintf("%sIf you believe this is an error with this tool, create a new issue:\n%s%s\n", padding, padding, issueUrl)
-}
