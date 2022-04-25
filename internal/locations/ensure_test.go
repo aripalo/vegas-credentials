280ae6e7788e982c70a59edb94ab8ff784a82497
Ari Palo <ari.palo@almamedia.fi>
2022-04-25 13:05:41 +0300

    test: ensure location

---
+++ internal/locations/ensure_test.go
@@ -0,0 +1,31 @@
+package locations
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/aripalo/vegas-credentials/internal/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLocatioEnsure(t *testing.T) {
+
+	t.Run("success", func(t *testing.T) {
+		tmpDir := os.TempDir()
+		appDir := filepath.Join(tmpDir, "vegas-credentials-testing-appdir")
+		_ = os.Remove(appDir)
+		actual := mustEnsureAppDir(appDir)
+		assert.Equal(t, filepath.Join(appDir, config.AppName), actual)
+
+		_, err := os.Stat(appDir)
+		assert.Equal(t, err, nil)
+
+		err = os.RemoveAll(appDir)
+		assert.Equal(t, err, nil)
+
+		_, err = os.Stat(appDir)
+		assert.Equal(t, os.IsNotExist(err), true)
+	})
+
+}
