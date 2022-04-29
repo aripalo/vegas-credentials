b1621f2bf8d59a651625cb5075be990251d93511
Ari Palo <ari.palo@almamedia.fi>
2022-04-30 00:49:18 +0300

    test: prettyjson marshal

--- internal/prettyjson/marshal_test.go
+++ internal/prettyjson/marshal_test.go
@@ -1,29 +1,49 @@
 package prettyjson
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 type testData struct {
-	BooleanFlag bool
-	StringFlag  string
+	BooleanValue bool
+	StringValue  string
 }
 
-func Test(t *testing.T) {
-
-	name := "pretty json marshal"
-	input := testData{
-		BooleanFlag: true,
-		StringFlag:  "value",
+func TestMarshal(t *testing.T) {
+	tests := []struct {
+		name       string
+		input      any
+		expected   string
+		errMessage string
+	}{
+		{
+			name:       "input unsupported",
+			input:      make(chan int),
+			expected:   "",
+			errMessage: "json: unsupported type: chan int",
+		},
+		{
+			name: "input valid",
+			input: testData{
+				BooleanValue: true,
+				StringValue:  "value",
+			},
+			expected: "{\n    \"BooleanValue\": true,\n    \"StringValue\": \"value\"\n}",
+		},
 	}
-	expected := "{\n    \"BooleanFlag\": true,\n    \"StringFlag\": \"value\"\n}"
 
-	t.Run(name, func(t *testing.T) {
-		actual, err := Marshal(input)
-		assert.Equal(t, err, nil)
-		assert.Equal(t, expected, actual)
-	})
+	for index, test := range tests {
 
+		name := fmt.Sprintf("case #%d - %s", index, test.name)
+		t.Run(name, func(t *testing.T) {
+			actual, err := Marshal(test.input)
+			if test.errMessage != "" {
+				assert.Equal(t, test.errMessage, err.Error())
+			}
+			assert.Equal(t, test.expected, actual)
+		})
+	}
 }
