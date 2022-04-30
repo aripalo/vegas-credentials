4b1e6d7f42b7df91ddfb4b82abb1d83e6283b251
Ari Palo <ari.palo@almamedia.fi>
2022-05-01 02:01:40 +0300

    test: version

---
+++ internal/application/version_test.go
@@ -0,0 +1,45 @@
+package application
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var version = "v0.0.0-development"
+
+func TestVersion(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    VersionFlags
+		expected string
+	}{
+		{
+			name:     "short version",
+			input:    VersionFlags{Full: false},
+			expected: fmt.Sprintf("%s\n", version),
+		},
+		{
+			name:     "full version",
+			input:    VersionFlags{Full: true},
+			expected: fmt.Sprintf("vegas-credentials version %s %s/%s\n", version, runtime.GOOS, runtime.GOARCH),
+		},
+	}
+
+	for index, test := range tests {
+
+		name := fmt.Sprintf("case #%d - %s", index, test.name)
+		t.Run(name, func(t *testing.T) {
+			var output bytes.Buffer
+			app := &App{dest: &output}
+			err := app.Version(test.input)
+			require.NoError(t, err)
+			actual := string(output.Bytes())
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
