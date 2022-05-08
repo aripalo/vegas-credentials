ca0016611dddc8dfa542dc18df784fc055aff08a
Ari Palo <ari.palo@almamedia.fi>
2022-05-09 00:19:05 +0300

    refactor: assume logic

--- internal/app/assume.go
+++ internal/app/assume.go
@@ -31,42 +31,50 @@ func (a *App) Assume(flags AssumeFlags) error {
 
 	creds := credentials.New(cfg)
 
-	if err = creds.Load(); err != nil {
-		msg.Debug("ℹ️", fmt.Sprintf("Credentials: Cache: %s", err))
-		msg.Debug("ℹ️", fmt.Sprintf("MFA: Serial: %s", cfg.MfaSerial))
-
-		code, err := totp.GetCode(context.Background(), totp.Options{
-			EnableGui:     !a.NoGui,
-			EnableYubikey: true, // TODO ??
-			YubikeySerial: cfg.YubikeySerial,
-			YubikeyLabel:  cfg.YubikeyLabel,
-		})
-
-		if err != nil {
-			msg.Fatal(fmt.Sprintf("TOTP: %s", err))
-		}
-
-		err = creds.New(code)
-		if err != nil {
-			if errors.Is(err, context.DeadlineExceeded) {
-				msg.Fatal(fmt.Sprintf("Operation Timeout"))
-			}
-			msg.Fatal(fmt.Sprintf("Credentials: STS: %s", err))
-		} else {
-			msg.Success("⏳", fmt.Sprintf("Credentials: New from STS, expiration in %s", humanize.Time(creds.Expiration)))
-		}
-	} else {
-
+	// Loading from cache is preferred, so if Temporary Session Credentials
+	// are found from cache then just return early.
+	if err = creds.Load(); err == nil {
 		msg.Success("⏳", fmt.Sprintf("Credentials: Loaded from cache, expiration in %s", humanize.Time(creds.Expiration)))
+		return exit(creds)
 	}
 
-	// TODO same for passwd cache
-	err = creds.Teardown()
+	msg.Debug("ℹ️", fmt.Sprintf("Credentials: Cache: %s", err))
+
+	code, err := totp.GetCode(context.Background(), totp.Options{
+		EnableGui:     !a.NoGui,
+		EnableYubikey: true, // TODO ??
+		YubikeySerial: cfg.YubikeySerial,
+		YubikeyLabel:  cfg.YubikeyLabel,
+	})
+
+	if err != nil {
+		msg.Fatal(fmt.Sprintf("TOTP: %s", err))
+	}
+
+	msg.Debug("ℹ️", fmt.Sprintf("MFA: Serial: %s", cfg.MfaSerial))
+
+	err = creds.New(code)
+
+	// Catch timeout error and return a cleaner error message.
+	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			msg.Fatal(fmt.Sprintf("Operation Timeout"))
+		}
+		msg.Fatal(fmt.Sprintf("Credentials: STS: %s", err))
+	}
+
+	msg.Success("⏳", fmt.Sprintf("Credentials: New from STS, expiration in %s", humanize.Time(creds.Expiration)))
+	return exit(creds)
+
+}
+
+// Shared "exit" handler
+func exit(creds *credentials.Credentials) error {
+	err := creds.Teardown()
 	if err != nil {
 		return err
 	}
 
 	msg.HorizontalRuler()
-
 	return creds.Output()
 }
